cf9d0ac2a02dfd051b041c724731127eaecab9e5
Dmitry Malko <31127521+open-projects@users.noreply.github.com>
2020-06-30 16:41:13 +0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-$(".check-all-table").click(function (e) {
-    $this = this;  
-    $.each($(this).parents('table').find('input'), function(i, item){
-    $(item).prop('checked', $this.checked);
-  });
-});
-
