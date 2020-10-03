a7530fff9aad3212ace3e9588dcefaa46dc00fd2
open-projects <dmitry.malko@gmail.com>
2020-10-03 14:19:53 +0300

    add app_name

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'project'
 urlpatterns = [
     path('get/<int:project_id>/', views.get, name='project_get'),
     path('set/<int:project_id>/', views.set, name='project_set'),
--- backend/run/urls.py
+++ backend/run/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'run'
 urlpatterns = [
     path('get/<int:run_id>/', views.run, name='run_get'),
 ]
