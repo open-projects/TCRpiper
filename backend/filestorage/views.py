64f46171ca3f3d082ee1b68928e450127616dde1
open-projects <dmitry.malko@gmail.com>
2020-11-06 14:33:37 +0300

    reconfigured js

--- backend/filestorage/views.py
+++ backend/filestorage/views.py
@@ -15,13 +15,18 @@ class FileForm(forms.ModelForm):
 class FileUploadView(View):
     def get(self, request):
         file_list = File.objects.all()
-        return render(self.request, 'filestorage.html', {'file_list': file_list})
+        context = {'file_list': file_list}
+        return render(self.request, 'filestorage.html', context)
 
     def post(self, request):
         form = FileForm(self.request.POST, self.request.FILES)
         if form.is_valid():
             data = form.save()
-            context = {'is_valid': True, 'name': data.file.name, 'url': data.file.url}
+            context = {
+                'is_valid': True,
+                'name': data.file.name,
+                'url': data.file.url
+            }
         else:
             context = {'is_valid': False}
         return JsonResponse(context)
